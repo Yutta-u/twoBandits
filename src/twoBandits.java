503efcf5b6cc87b4a90452aba9cd0c7af7343b07
Yutta <111496203+Yutta-u@users.noreply.github.com>
2022-08-30 20:17:40 +0500

    Update twoBandits.java

--- src/twoBandits.java
+++ src/twoBandits.java
@@ -5,9 +5,8 @@ public class twoBandits {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
-        int a1 = 10 - a;
-        int b1 = 10 - b;
-        System.out.print(a1);
-        System.out.print(" " + b1);
+        int a1 = a - 1;
+        int b1 = b - 1;
+        System.out.print(b1 + " " + a1);
     }
 }
